ec77706bd3da35ce532916cd0ede1674280fa002
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-01-26 19:11:51 +0200

    Initial Commit

---
+++ PhotoGallery/PhotoGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGalleryApp.swift
+//  PhotoGallery
+//
+//  Created by test on 26.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
